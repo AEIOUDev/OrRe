f2ba9b833f8ffab3ef4db6a08bf2f4ccbf2679a2
MyKnow <myknow000@gmail.com>
2024-02-29 22:42:59 +0900

    간단한 웨이팅 화면

--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:url_strategy/url_strategy.dart';
 
-// A Counter example implemented with riverpod
-
-class CounterNotifier extends StateNotifier<int> {
-  CounterNotifier(this.ref) : super(0);
-
-  final Ref ref;
-
-  Future<void> increment() async {
-    state++;
-  }
-
-  Future<void> decrement() async {
-    state--;
-  }
-}
-
-final counterStateProvider = StateNotifierProvider<CounterNotifier, int>((ref) {
-  return CounterNotifier(ref);
-});
+import 'reservation.dart';
 
 void main() {
-  runApp(
-    // Adding ProviderScope enables Riverpod for the entire project
-    const ProviderScope(child: RiverpodCounterApp()),
-  );
+  setPathUrlStrategy(); // 해시(#) 없이 URL 사용
+  runApp(MyApp());
 }
 
-class RiverpodCounterApp extends StatelessWidget {
-  const RiverpodCounterApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: CounterScreen());
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      onGenerateRoute: (settings) {
+        // 경로 이름 파싱
+        Uri uri = Uri.parse(settings.name!);
+        // '/reservation/{가게코드}' 경로 처리
+        if (uri.pathSegments.length == 2 &&
+            uri.pathSegments.first == 'reservation') {
+          String storeCode = uri.pathSegments[1];
+          return MaterialPageRoute(
+              builder: (context) => ReservationPage(storeCode: storeCode));
+        }
+
+        // 다른 경로는 여기에서 처리
+        // 기본적으로 홈페이지로 리다이렉트
+        return MaterialPageRoute(builder: (context) => HomePage());
+      },
+    );
   }
 }
 
-class CounterScreen extends ConsumerWidget {
-  const CounterScreen({super.key});
-
+class HomePage extends StatelessWidget {
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final count = ref.watch(counterStateProvider);
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Counter example')),
-      body: Center(
-        child: Text(
-          '$count',
-          style: const TextStyle(fontSize: 32.0),
-        ),
+      appBar: AppBar(
+        title: Text("Home Page"),
       ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            key: const Key('counterView_increment_floatingActionButton'),
-            child: const Icon(Icons.add),
-            onPressed: () =>
-                ref.read(counterStateProvider.notifier).increment(),
-          ),
-          const SizedBox(height: 8),
-          FloatingActionButton(
-            key: const Key('counterView_decrement_floatingActionButton'),
-            child: const Icon(Icons.remove),
-            onPressed: () =>
-                ref.read(counterStateProvider.notifier).decrement(),
-          ),
-        ],
+      body: Center(
+        child: Text("Welcome to the Home Page"),
       ),
     );
   }
