05aeddf85f711313760e50a883a0b24e904a3ad7
MyKnow <myknow000@gmail.com>
2024-03-04 10:41:32 +0900

    기능추가: FCM으로 푸시 알림 받는 기능

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,50 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:url_strategy/url_strategy.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 import 'presenter/reservation.dart';
 import 'presenter/store_waiting_widget.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'services/notifications_services.dart'; // Firebase 초기화 옵션을 포함한 파일
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Flutter 엔진과 위젯 바인딩을 초기화
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ); // Firebase를 현재 플랫폼에 맞게 초기화
+
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: true,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+  print('User granted permission: ${settings.authorizationStatus}');
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  print(fcmToken);
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage? message) {
+    if (message != null) {
+      if (message.notification != null) {
+        print(message.notification!.title);
+        print(message.notification!.body);
+        print(message.data["click_action"]);
+      }
+    }
+  });
+
+  final notificationService = NotificationService();
+  notificationService.listenNotifications();
+
   setPathUrlStrategy(); // 해시(#) 없이 URL 사용
+
   runApp(ProviderScope(child: MyApp()));
 }
 
@@ -27,10 +65,8 @@ class MyApp extends StatelessWidget {
             uri.pathSegments.first == 'reservation') {
           String storeCode = uri.pathSegments[1];
           return MaterialPageRoute(
-              // builder: (context) => ReservationPage(storeCode: storeCode));
               builder: (context) => WaitingInfoWidget(storeCode: storeCode));
         }
-
         // 다른 경로는 여기에서 처리
         // 기본적으로 홈페이지로 리다이렉트
         return MaterialPageRoute(builder: (context) => HomePage());
@@ -39,7 +75,21 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late Future<String?> fcmToken;
+
+  @override
+  void initState() {
+    super.initState();
+    fcmToken =
+        FirebaseMessaging.instance.getToken(); // Fetch the token asynchronously
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,7 +97,22 @@ class HomePage extends StatelessWidget {
         title: Text("Home Page"),
       ),
       body: Center(
-        child: Text("Welcome to the Home Page"),
+        // Use FutureBuilder to wait for the FCM token
+        child: FutureBuilder<String?>(
+          future: fcmToken,
+          builder: (context, snapshot) {
+            // Check if the future is complete and has data
+            if (snapshot.connectionState == ConnectionState.done &&
+                snapshot.hasData) {
+              return Text("FCM Token: ${snapshot.data}");
+            } else if (snapshot.hasError) {
+              return Text("Error fetching FCM Token");
+            } else {
+              // Show a loading indicator while waiting for the token
+              return CircularProgressIndicator();
+            }
+          },
+        ),
       ),
     );
   }
