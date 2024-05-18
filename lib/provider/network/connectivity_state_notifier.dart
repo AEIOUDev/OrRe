2fe4c84cebdabdcfc09c64ef7cd290ff479edd1f
MyKnow <myknow000@gmail.com>
2024-05-18 19:18:11 +0900

    [24.05.18] Go Router 적용 전

--- lib/provider/network/connectivity_state_notifier.dart
+++ lib/provider/network/connectivity_state_notifier.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:get/get.dart';
 import 'package:internet_connectivity_checker/internet_connectivity_checker.dart';
 import 'package:orre/provider/error_state_notifier.dart';
+import 'package:orre/widget/popup/alert_popup_widget.dart';
 
 final networkStateProvider = Provider<Stream<bool>>((ref) {
   return ConnectivityChecker(interval: const Duration(seconds: 5)).stream;
@@ -24,12 +27,12 @@ class NetworkStateNotifier extends StateNotifier<bool> {
         state = isConnected;
         if (isConnected) {
           print("networkStateNotifierProvider is connected");
-          ref.read(errorStateNotifierProvider.notifier).addError(Error.network);
-        } else {
-          print("networkStateNotifierProvider is disconnected");
           ref
               .read(errorStateNotifierProvider.notifier)
               .deleteError(Error.network);
+        } else {
+          print("networkStateNotifierProvider is disconnected");
+          ref.read(errorStateNotifierProvider.notifier).addError(Error.network);
         }
       }
     });
